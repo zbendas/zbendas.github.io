5a897eef42036c4a5efdd2cbefb57a918cf5342a
Zachariah Bendas <zbendas@yahoo.com>
2018-03-15 22:37:59 -0500

    Modify behavior for closing hamburger menu
    Slightly update projects page visuals as well.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,16 @@
 $(document).ready(() => {
     let $toggleButton = $('.hamburger-button'),
         $buttonBars = $toggleButton.find('.hamburger-bar'),
-        $menuContent = $('.menu-content');
+        $menuContent = $('.menu-content'),
+        $mainContent = $('.grid-container');
     $toggleButton.on('click', () => {
         $buttonBars.toggleClass('close-button');
         $menuContent.toggleClass('menu-show');
+    });
+    $mainContent.on('click', () => {
+       if ($menuContent.hasClass('menu-show')){
+           $buttonBars.toggleClass('close-button');
+           $menuContent.toggleClass('menu-show');
+       }
     });
 });
